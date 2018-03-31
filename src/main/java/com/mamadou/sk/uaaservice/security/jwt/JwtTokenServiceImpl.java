0502f01db45307fd17ae668a21f83b2cf4537d86
barry <mamadou.barry29@mail.dcu.ie>
2018-03-31 22:34:57 +0100

    adding unit test
    TODO jwt token validation
    TODO code clean up
    TODO spring contract integration
    TODO swagger integration
    TODO Docker integration

---
+++ src/main/java/com/mamadou/sk/uaaservice/security/jwt/JwtTokenServiceImpl.java
@@ -0,0 +1,116 @@
+package com.mamadou.sk.uaaservice.security.jwt;
+
+import com.mamadou.sk.uaaservice.security.user.CustomUserDetails;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.lang.Strings;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Component
+@Slf4j
+public class JwtTokenServiceImpl implements JwtTokenService {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private ZonedDateTime now = ZonedDateTime.now();
+
+    @Override
+    public String generate(Authentication authentication) {
+        CustomUserDetails user = (CustomUserDetails)authentication.getPrincipal();
+        return Jwts.builder()
+                   .setIssuer(applicationName)
+                   .setIssuedAt(createdDate())
+                   .setSubject(user.getUsername())
+                   .claim("auth", authorities(user.getAuthorities()))
+                   .setExpiration(expirationDate())
+                   .signWith(SignatureAlgorithm.HS512, secret)
+                   .compact();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Authentication getAuthentication(String jwt) {
+        Claims body =(Claims) Jwts.parser()
+                                  .setSigningKey(secret)
+                                  .parse(jwt)
+                                  .getBody();
+        List<String> authorityClaim = ((List<String>) body.get("auth"));
+        List<SimpleGrantedAuthority> authorities = authorityClaim.stream()
+                                                                 .map(SimpleGrantedAuthority::new)
+                                                                 .collect(Collectors.toList());
+        return new UsernamePasswordAuthenticationToken(body.getSubject(), jwt, authorities);
+    }
+
+    @Override
+    public boolean isTokenValid(String jwtToken) {
+        try {
+            Jwts.parser().setSigningKey(secret).parse(jwtToken);
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.info("Token have expired", e);
+        } catch (MalformedJwtException e) {
+            log.info("Token is malformed");
+        } catch (SignatureException e) {
+            log.info("Token Signature could be verified");
+        } catch (IllegalArgumentException e) {
+            log.info("token is missing or empty");
+        }
+        return false;
+    }
+
+    /**
+     * convert List of GrantedAuthority to an array of String
+     *
+     * @param authorities - user granted authorities
+     * @return array of authorities
+     */
+    private String[] authorities(Collection<? extends GrantedAuthority> authorities) {
+        return Strings.toStringArray(authorities.stream()
+                                                .map(GrantedAuthority::getAuthority)
+                                                .collect(Collectors.toList()));
+    }
+
+    /**
+     * set token expiration date
+     *
+     * @return expiration date
+     */
+    private Date expirationDate() {
+        ZonedDateTime expirationDate = now.plusMinutes(expiration);
+        return Date.from(expirationDate.toInstant());
+    }
+
+    /**
+     * get token issued date
+     *
+     * @return issued date
+     */
+    private Date createdDate() {
+        return Date.from(now.toInstant());
+    }
+
+}
---
+++ src/test/java/com/mamadou/sk/uaaservice/security/jwt/JwtTokenServiceImplTest.java
@@ -0,0 +1,218 @@
+package com.mamadou.sk.uaaservice.security.jwt;
+
+import com.mamadou.sk.uaaservice.security.user.CustomUserDetails;
+import com.mamadou.sk.uaaservice.user.entitity.Authority;
+import com.mamadou.sk.uaaservice.user.entitity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.assertj.core.util.Lists;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class JwtTokenServiceImplTest {
+    private static final String INVALID_TOKEN_SECRET = "INVALID_SECRET";
+    private static final String ROLE_ADMIN = "ROLE_ADMIN";
+    private static final String ADMIN_USERNAME = "admin";
+    private static final String TOKEN_ISSUER = "uaa-service";
+    private static final String TOKEN_SECRET = "secret";
+    private static final long TOKEN_EXPIRATION = 5L;
+    @Mock
+    private Authentication authentication;
+    private JwtTokenService jwtTokenService;
+
+    @Before
+    public void setUp() {
+        jwtTokenService = new JwtTokenServiceImpl();
+
+        // setup environment variable that are required by the JwtService
+        ReflectionTestUtils.setField(jwtTokenService, "applicationName", TOKEN_ISSUER);
+        ReflectionTestUtils.setField(jwtTokenService, "expiration", TOKEN_EXPIRATION);
+        ReflectionTestUtils.setField(jwtTokenService, "secret", TOKEN_SECRET);
+
+        // set user new authenticated used
+        User admin = new User().setUsername(ADMIN_USERNAME)
+                               .setAuthorities(Lists.newArrayList(new Authority(ROLE_ADMIN)));
+        given(authentication.getPrincipal()).willReturn(new CustomUserDetails(admin));
+    }
+
+    @Test
+    public void generate_shouldAddApplicationNameAsIssuedBy() {
+        // when
+        String generateToken = jwtTokenService.generate(authentication);
+        String issuedBy = getParsedTokenBody(generateToken).getIssuer();
+
+        // then
+        assertThat(issuedBy).isEqualTo(TOKEN_ISSUER);
+    }
+
+    @Test
+    public void generate_shouldAddTokenCreationDate() {
+        // given
+        Instant instant = ZonedDateTime.now().toInstant();
+        // when
+        String generateToken = jwtTokenService.generate(authentication);
+        Date issuedAt = getParsedTokenBody(generateToken).getIssuedAt();
+
+        // then
+        assertThat(issuedAt).isEqualToIgnoringMillis(Date.from(instant));
+    }
+
+    @Test
+    public void generate_shouldAddUsernameAsSubject_whenAuthenticationIsSetup() {
+        // when
+        String generateToken = jwtTokenService.generate(authentication);
+        String username = getParsedTokenBody(generateToken).getSubject();
+
+        // then
+        assertThat(username).isEqualTo(ADMIN_USERNAME);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void generate_shouldAddUserAuthorities_whenAuthenticationIsSetup() {
+        // when
+        String generateToken = jwtTokenService.generate(authentication);
+        List<String> authorities = (List<String>) getParsedTokenBody(generateToken).get("auth");
+
+        // then
+        assertThat(authorities).containsExactly(ROLE_ADMIN);
+    }
+
+    @Test
+    public void generate_shouldAddTokenExpirationDate() {
+        // given
+        Instant instant = ZonedDateTime.now()
+                                       .plusMinutes(TOKEN_EXPIRATION)
+                                       .toInstant();
+        // when
+        String generateToken = jwtTokenService.generate(authentication);
+        Date issuedAt = getParsedTokenBody(generateToken).getExpiration();
+
+        // then
+        assertThat(issuedAt).isEqualToIgnoringMillis(Date.from(instant));
+    }
+
+    @Test
+    public void getAuthentication_shouldPopulateAuthenticationWithUsernameFromToken() {
+        // given
+        String token = jwtTokenService.generate(authentication);
+
+        // when
+        Authentication usernamePasswordAuthenticationToken = jwtTokenService.getAuthentication(token);
+        String username = (String) usernamePasswordAuthenticationToken.getPrincipal();
+
+        // then
+        assertThat(username).isEqualTo(ADMIN_USERNAME);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void getAuthentication_shouldPopulateAuthenticationWithAuthoritiesFromToken() {
+        // given
+        String token = jwtTokenService.generate(authentication);
+
+        // when
+        Authentication usernamePasswordAuthenticationToken = jwtTokenService.getAuthentication(token);
+        List<SimpleGrantedAuthority> authorities = (List<SimpleGrantedAuthority>) usernamePasswordAuthenticationToken.getAuthorities();
+
+        // then
+        assertThat(authorities).isEqualTo(AuthorityUtils.createAuthorityList(ROLE_ADMIN));
+    }
+
+
+    @Test
+    public void isTokenValid_shouldReturnTrue_whenTokenIsValid() {
+        // given
+        String validToken = jwtTokenService.generate(authentication);
+
+        // when
+        boolean tokenValid = jwtTokenService.isTokenValid(validToken);
+
+        // then
+        assertThat(tokenValid).isTrue();
+    }
+
+    @Test
+    public void isTokenValid_shouldReturnFalse_whenTokenAlreadyExpired() {
+        // given
+        // set up token to 0 so that it will expire by the time we
+        ReflectionTestUtils.setField(jwtTokenService, "expiration", 0L);
+
+        String expiredToken = jwtTokenService.generate(authentication);
+
+        // when
+        boolean tokenValid = jwtTokenService.isTokenValid(expiredToken);
+
+        // then
+        assertThat(tokenValid).isFalse();
+    }
+
+    @Test
+    public void isTokenValid_shouldReturnFalse_whenTokenIsMalFormed() {
+        // given
+        String malformedToken = "eyJhbGciOiJIUzUxMiJ9..-bA";
+
+        // when
+        boolean tokenValid = jwtTokenService.isTokenValid(malformedToken);
+
+        // then
+        assertThat(tokenValid).isFalse();
+    }
+
+    @Test
+    public void isTokenValid_shouldReturnFalse_whenTokenHaveDifferentSecretKey() {
+        // given
+        String validToken = jwtTokenService.generate(authentication);
+
+        // change token secret key
+        ReflectionTestUtils.setField(jwtTokenService, "secret", INVALID_TOKEN_SECRET);
+
+        // when
+        boolean tokenValid = jwtTokenService.isTokenValid(validToken);
+
+        // then
+        assertThat(tokenValid).isFalse();
+
+    }
+
+    @Test
+    public void isTokenValid_shouldReturnFalse_whenTokenIsNull() {
+        // when
+        boolean tokenValid = jwtTokenService.isTokenValid(null);
+
+        // then
+        assertThat(tokenValid).isFalse();
+    }
+
+    @Test
+    public void isTokenValid_shouldReturnFalse_whenTokenIsEmpty() {
+        // when
+        boolean tokenValid = jwtTokenService.isTokenValid("");
+
+        // then
+        assertThat(tokenValid).isFalse();
+    }
+
+    private Claims getParsedTokenBody(String generateToken) {
+        return (Claims) Jwts.parser()
+                            .setSigningKey(TOKEN_SECRET)
+                            .parse(generateToken)
+                            .getBody();
+    }
+}
