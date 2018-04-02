d80716b59d88960998cbc46b96646d8e51e3fe6a
barry <mamadou.barry29@mail.dcu.ie>
2018-04-02 14:35:29 +0100

    adding create implementation
    TODO code clean up
    TODO spring contract integration
    TODO swagger integration
    TODO Docker integration

--- src/main/resources/dev/db/migration/h2/V5__create_admin_user.sql
+++ src/main/resources/dev/db/migration/h2/V5__create_admin_user.sql
@@ -1,5 +1,5 @@
 insert into user(user_id,first_name,last_name,username,password,email,enabled,expired,locked)
 values(1,'first','last','admin','$2a$10$Noxuv0dfNT.O68B4hgginevx4MhTCR//r3hOKbMGN6AIhJPa4na7.','admin@test.com',true,false,false);
 
-insert into user_authority(user_id, role)
-values(1,'ROLE_ADMIN')
+insert into user_authority(user_id, role)values(1,'ROLE_ADMIN');
+insert into user_authority(user_id, role)values(1,'ROLE_USER')
